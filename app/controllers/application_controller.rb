f2d5fe989d4b91f0bf5f03831c104350f6092bb2
lmaconde <luismaconde@gmail.com>
2016-11-14 18:38:43 +0000

    happy birthday

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: '<h1 style="text-align:center; width: 100vw; font-family: helvetica, sans-serif;font-size: 15vw;">PARABÉNS<br>CRISTINA!<br>🎉🎉🎉</h1>'.html_safe
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def hello
+    render html: "hello, world!"
+  end
 end
